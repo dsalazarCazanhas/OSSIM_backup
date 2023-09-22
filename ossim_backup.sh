534fd219f38b011eafde4e2e64259e1f8d10b355
dsalazarCazanhas <dsalazarcazanhas@gmail.com>
2023-09-22 07:55:05 -0400

    Rsync config

--- ossim_backup.sh
+++ ossim_backup.sh
@@ -1,22 +1,46 @@
 #!/bin/sh
 
-# Checking if the programm was executed previously
-function first_check 
-{
-if [ -e /var/log/backup_script_errors ]
+# Check for backup user
+if [ ! cat /etc/passwd | cut -d ':' -f 1=='ubackup' ]
 then
-  go_on
+	useradd ubackcup
+fi
+
+# Check if there are backups
+if [ ! -e /var/alienvault/backup ]
+then
+	exit
+fi
+# Checking if the script was executed previously
+if [ -e /var/alienvault/backup_script_errors ]
+then
+	compress
 else
-mkdir /var/log/backup_script_errors
-  go_on
+	mkdir /var/alienvault/backup_script_errors
+	compress
+fi
+
+# Vars
+folderror=/var/alienvault/backup_script_errors
+
+function compress
+{
+ tar -cJf /tmp/$HOSTNAME_$(date +%F)_backup.tar.xz /var/alienvault/backup/*configuration*$'gz' 2>$folderror/$(date +%F)_error.log
+}
+# Rsync
+function saveit
+{
+if[ -e /root/.ssh ]
+then
+	backup_user="dsalazar"
+	backup_ip="192.168.65.178"
+	path_to_backup="~/ossim_backup"
+	rsync /tmp/*backup*$'xz' $backup_user@$backup_ip:$path_to_backup
+else
+	ssh-keygen -t rsa -C "OSSIM backup"
+	ssh-copy-id user@hostip
 fi
 }
-#
-function go_on
-{
-  tar -cJf $folder/$HOSTNAME_$(date +%F)_backup.tar.xz /usr/lib/ossim/backup/*setup* 2>$folderror/$(date +%F)_error.log
-}
-#
-mkdir /tmp/script_griffith
-folder=/tmp/script_griffith
-folderror=/var/log/backup_script_errors
+# Execution
+compress
+saveit
